13fac9b9bfda380cd007401d1cd53600dc75019c
brianfreudman <55546075+brianfreudman@users.noreply.github.com>
2019-10-04 15:49:13 -0400

    Add files via upload
    Update of the main file including Jose changes

--- main.py
+++ main.py
@@ -2,21 +2,35 @@ print("House buying app")
 
 print("Please Log In")
 
-#username=input("Please enter your username >> ")
-#password-input("Please enter your password >> ")
+#username=input("please enter your username >> ")
+#password-input("pleasde enter your password >> ")
 
-#counter=int(input("How many houses do you want to store? >> "))
+counter=int(input("how many houses do you want to insert >> "))
 
-#while True 
-housesp=int(input("Please enter the house buying price >> "))
-monthl=int(input("Please submit the amount of months that you have owned the house >> ")) 
+while True:
+ if (counter) >= 1: 
 
-realtyFee=((housesp) * (.06))
-loanFee=(((monthl* .0083) * (housesp)))
-titleFee=((housesp) * .03)
-total=((housesp) + (realtyFee) + (loanFee) + (titleFee))
-print("The sale price is", housesp)
-print("The realty fee is", realtyFee)
-print("The loan fee is", loanFee)
-print("The title fee is", titleFee)
-print("The sales price is", total)
+    
+    housesp=float(input("Please enter the house buying price >> "))
+    monthl=int(input("Please submit the amount of months that you have owned the house >> ")) 
+    creditCardNumber=int(input("Please enter your credit card number >> "))
+    creditCardExpiration=int(input("Please enter your credit card expiration date >> "))
+    creditCardSecurity=int(input("Please enter your credit card security code >> "))
+
+    realtyFee=((housesp) * (.06))
+    loanFee=(((monthl* .0083) * (housesp)))
+    titleFee=((housesp) * .03)
+    total=((housesp) + (realtyFee) + (loanFee) + (titleFee))
+    print("The sale price is ")
+    print (housesp)
+    print("The realty fee is")
+    print(realtyFee)
+    print("The loan fee is ")
+    print (loanFee)
+    print("The title fee is")
+    print (titleFee)
+    print("The sales price is")
+    print(total)
+    counter-=1
+else :
+    print("Done")
